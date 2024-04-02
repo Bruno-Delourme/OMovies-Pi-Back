715874d9c3f826627ad1974709e64dc6e056ceb0
GwendolineFAVRE-FELIX <gwendoline.favre-felix@oclock.school>
2024-04-02 15:14:46 +0200

    fake_data3

--- scripts/4.fake_data.sql
+++ scripts/4.fake_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO "user"
-(id, pseudo, email, date_of_birth_ hashed_password)
+("pseudo", "email", "date_of_birth", "hashed_password")
 VALUES
-('1', "Mathias", "mathias@front.fr", "1989/06/23", "mathias"),
-('2', "Fadwa", "fadwa@front.fr", "1990/02/25", "fadwa"),
-('3', "Bruno", "bruno@front.fr", "1889/07/17", "bruno"),
-('4', "Gwen", "gwen@back.fr", "1991/05/24", "gwen");
+('mathias', 'mathias@front.fr', '1989/06/23', 'mathias'),
+('fadwa', 'fadwa@front.fr', '1990/02/25', 'fadwa'),
+('bruno', 'bruno@front.fr', '1889/07/17', 'bruno'),
+('gwen', 'gwen@back.fr', '1991/05/24', 'gwen');
