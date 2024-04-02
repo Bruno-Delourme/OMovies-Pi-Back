23408057c2639f856a765217610b885b91327c02
GwendolineFAVRE-FELIX <gwendoline.favre-felix@oclock.school>
2024-04-02 15:09:20 +0200

    fake_data2

--- scripts/4.fake_data.sql
+++ scripts/4.fake_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO "user"
 (id, pseudo, email, date_of_birth_ hashed_password)
 VALUES
-('1', "Mathias", "mathias@front.fr", "1989/06/23", "$2b$10$lpb5xkV4R5FtXYoIKwByUeFsnzad.ptDUzv.h.0tNmnQPMgS2K1AB"),
-('2', "Fadwa", "fadwa@front.fr", "1990/02/25", "$2b$10$lpb5xkV4R5FtXYoIKwByUeFsnzad.ptDUzv.h.0tNmnQPMgS2K1CD"),
-('3', "Bruno", "bruno@front.fr", "1889/07/17", "$2b$10$lpb5xkV4R5FtXYoIKwByUeFsnzad.ptDUzv.h.0tNmnQPMgS2K1EF"),
-('4', "Gwen", "gwen@back.fr", "1991/05/24", "$2b$10$lpb5xkV4R5FtXYoIKwByUeFsnzad.ptDUzv.h.0tNmnQPMgS2K1GF");
+('1', "Mathias", "mathias@front.fr", "1989/06/23", "mathias"),
+('2', "Fadwa", "fadwa@front.fr", "1990/02/25", "fadwa"),
+('3', "Bruno", "bruno@front.fr", "1889/07/17", "bruno"),
+('4', "Gwen", "gwen@back.fr", "1991/05/24", "gwen");
