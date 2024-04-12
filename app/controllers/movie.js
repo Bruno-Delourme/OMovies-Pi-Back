18c3031937a4621f008823cc63a71e01040f1af4
GwendolineFAVRE-FELIX <gwendoline.favre-felix@oclock.school>
2024-04-12 13:13:19 +0200

    providers in fetchById

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -44,7 +44,45 @@ const movieController = {
   
       // Parse the credits response into JSON format
       const credits = await creditsResponse.json();
-  
+
+        // Add viewing platform information
+        const providersResponse = await fetch(`${process.env.API_TMDB_BASE_URL}movie/${movieId}/watch/providers?api_key=${process.env.API_TMDB_KEY}&region=FR`);
+        
+        if (!providersResponse.ok) {
+            throw new Error('Erreur réseau ou réponse invalide lors de la récupération des plateformes de visionnage');
+        }
+
+        const providersData = await providersResponse.json();
+        
+        movie.providers = [];
+        
+        // Check if information is available for France
+        if (providersData.results.FR) {
+          const franceProviders = providersData.results.FR;
+
+          // Browse the keys of the franceProviders object
+          for (const offerType in franceProviders) {
+              if (Object.hasOwnProperty.call(franceProviders, offerType)) {
+                  const offers = franceProviders[offerType];
+
+                  // Check if the offer is an array
+                  if (Array.isArray(offers)) {
+                      // Add the names of content providers for each offer type
+                      offers.forEach(provider => {
+                          if (provider.provider_name) {
+                              movie.providers.push(provider.provider_name);
+                          }
+                      });
+                  } else {
+                      // If it is not a table, add the supplier name directly
+                      if (offers.provider_name) {
+                          movie.providers.push(offers.provider_name);
+                      };
+                  };
+              };
+          };
+        };
+
       // Add credits information to the movie object
       movie.credits = credits;
   
